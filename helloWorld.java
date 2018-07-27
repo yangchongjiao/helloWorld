f2bc790eb4dd4642d4eb6a0f55056e7f9fdb8432
helloWorld <y648022352@163.com>
2018-07-28 02:29:23 +0800

    Signed-off-by: helloWorld <y648022352@163.com>

--- helloWorld.java
+++ helloWorld.java
@@ -2,7 +2,7 @@ public class helloWorld {
 	public static void main (String args[]){
 		System.out.println("helloWorld!!");
 		System.out.println("helloWorld22222!!");
-		for(int i-0;i<10;i++){
+		for(int i=0;i<10;i++){
 			
 		System.out.println("helloWorld2----"+i);
 		}
